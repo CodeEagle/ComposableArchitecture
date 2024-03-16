095c8d0a5d7a8ca7b6b0bc6f2130846ff157905b
Lincoln <Stasura@hotmail.com>
2024-03-16 17:06:48 +0800

    change: fix arch

--- .nuget/pack.sh
+++ .nuget/pack.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # This script is used to generate the package for the ComposableArchitecture.
 KEY=`echo $NUGET_API_KEY`
-VERSION="0.0.7"
+VERSION="0.0.14" 
 # replace the version in the nuspec file
 sed -i '' "s/<version>.*<\/version>/<version>$VERSION<\/version>/g" ComposableArchitecture.nuspec
+cd ..
 sed -i '' "s/<Version>.*<\/Version>/<Version>$VERSION<\/Version>/g" ComposableArchitecture.csproj
-echo "Publishing ComposableArchitecture..."
-dotnet publish
+echo "Publishing ComposableArchitecture..." 
+dotnet publish -c Release 
 echo "Packing ComposableArchitecture..."
+cd -
 nuget pack ComposableArchitecture.nuspec
 echo "Pushing ComposableArchitecture..."
-dotnet nuget push "bin/Release/SelfStudio.ComposableArchitecture.$VERSION.nupkg"  -k $KEY --source  https://api.nuget.org/v3/index.json
+dotnet nuget push "SelfStudio.ComposableArchitecture.$VERSION.nupkg"  -k $KEY --source  https://api.nuget.org/v3/index.json
